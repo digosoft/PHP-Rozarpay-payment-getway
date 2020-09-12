ecda10300923b0f2ea2b1293268d1e7f1ec46db4
Jay Kumar <jay@digosoft.com>
2020-09-12 23:12:57 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId = 'rzp_test_IwgQhPTyrXJaS4';
+$keySecret = 'KCdiqySg3Ua4K5AVbxhQdD0m';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
